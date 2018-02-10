9576d43b2259ffbdf44a89a9a3ac2d0ccd64d11b
JohnStein01 <andro.john01@gmail.com>
2018-02-10 11:56:56 +0200

    Try2

---
+++ src/com/aksu/grup32java/Uygulama.java
@@ -0,0 +1,20 @@
+package com.aksu.grup32java;
+
+import com.aksu.grup32java.ders3.Ders3;
+import java.util.Scanner;
+
+public class Uygulama {
+
+    public static void main(String[] args) {
+
+        Ders3 ders3 = new Ders3();
+        ders3.ornekMetod();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yaşınızı Giriniz");
+        int yas = scanner.nextInt();
+        ders3.parametreliMethod(yas);
+
+    }
+
+}
---
+++ src/com/aksu/grup32java/ders3/Ders3.java
@@ -0,0 +1,18 @@
+package com.aksu.grup32java.ders3;
+
+public class Ders3 {
+
+    public void ornekMetod(){
+        System.out.println("Örnek Metod Çalıştı");
+
+    }
+
+    public void parametreliMethod(int yas){
+        int mevcutYil=2018;
+        int dogumYılı=mevcutYil-yas;
+
+        System.out.println("Doğum Yiliniz "+dogumYılı);
+
+    }
+
+}
